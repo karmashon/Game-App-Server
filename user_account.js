bcd7202a4949980750a1fbba031bcb8468cf82df
Rushil A Krishnan <rushilben10@hotmail.com>
2023-09-28 23:14:45 +0530

    Fixed connection to cloud db

--- user_account.js
+++ user_account.js
@@ -8,28 +8,7 @@ const app = express();
 
 
 // Create a new sequelize instance with mysql database info
-const sequelize = new sequelizer.Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PWD,
-{
-    host:process.env.DB_HOST,
-    dialect:"mysql"
-});
-
-// Define a user model
-const UserLogin = sequelize.define("UserLogin",
-{
-    uname:
-    {
-        type:sequelizer.DataTypes.STRING,
-        allowNull:false,
-        primaryKey:true
-    },
-    password:
-    {
-        type:sequelizer.DataTypes.STRING,
-        allowNull:false
-    }
-
-});
+const sequelize = new sequelizer.Sequelize(process.env.DB_URL);
 
 // Create a function to sync table.
 // It is asynchronous as table syncing takes time.
@@ -48,61 +27,57 @@ async function syncTable(){
     }
 }
 
-// Call initializeTable() to sync the table with database, and wait.
-// Once it is complete, initiate the process of adding record by calling create() and wait.
+// Test the database connection and sync the table model
+sequelize.authenticate()
+.then(function()
+{
+    console.log("Connection successful");
+    syncTable();
+})
+.catch(function(err)
+{
+    console.log("Error in connecting to database: "+err);
+});
+
+// Return a model on which table queries can be performed
+const UserLogin = sequelize.define("UserLogin",
+{
+    uname:
+    {
+        type:sequelizer.DataTypes.STRING,
+        allowNull:false,
+        primaryKey:true
+    },
+    password:
+    {
+        type:sequelizer.DataTypes.STRING,
+        allowNull:false
+    }
+
+});
+
+// Initiate the process of adding record by calling create() and wait.
+// Return the newly created record
 async function insertRecord(newName,newPwd)
 {
-    // Wait for table to sync.
-    var tableSynced = await syncTable();
-    if (tableSynced){
-        try
-        {
-            // Wait for table to add record
-            var newUser = await UserLogin.create({uname:newName,password:newPwd});
-            console.log("Inserted record with username: "+newName+" and password: "+newPwd);
-            return newUser;
-        }
-        catch(err)
-        {
-            console.log("Insert Error for Record with username "+newName+": "+err);
-            return null;
-        }
-    }
+    var newUser = await UserLogin.create({uname:newName,password:newPwd});
+    console.log("Inserted record with username: "+newName+" and password: "+newPwd);
+    return newUser;
+
 }
-async function displayAllRecords(){
-    try{
-        const userList = await UserLogin.findAll();
-        console.log(userList);
-    }
-    catch(err)
-    {
-        console.log("Display Error: "+err);
-    }
-}
-/*async function displayRecord(name){
-    try{
-        const userList = await UserLogin.findAll();
-        console.log(userList);
-    }
-    catch(err)
-    {
-        console.log("Display Error: "+err);
-    }
-}*/
 
 
 // Run all database query operations in async function to use await freely.
 // This allows waiting for insert statements to be waited for, before displaying
 async function main(){
+    
     app.use(bodyParser.json());
 
     app.listen(port, function(err){
         console.log("Server listening on port: "+port); 
     });
 
-    app.get("/*",function(req,res){
-        res.end("Server running on port: "+port);
-    });
+    // Endpoint for register
     app.post("/addUser",async function(req,res){
         var newName = req.body.uname;
         var newPwd = req.body.password + pwdSalt;
@@ -119,6 +94,7 @@ async function main(){
         res.end();
     });
 
+    // Endpoint for login
     app.post("/verifyUser",async function(req,res){
         var newName = req.body.uname;
         var newPwd = req.body.password;
@@ -134,5 +110,4 @@ async function main(){
 
 
 }
-
 main();
